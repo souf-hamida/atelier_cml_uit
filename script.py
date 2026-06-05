0e5e51fb8c22221e08f9887d4aab0073dee2219a
HAMIDA <hamida@test.ma>
2026-06-05 20:10:38 +0100

    First commit 2

--- script.py
+++ script.py
@@ -36,7 +36,7 @@ TRAIN_PATH = os.path.join(os.getcwd(), 'dataset.csv')
 df = pd.read_csv(TRAIN_PATH)
 
 ## Drop first 3 features
-df.drop(columns=['RowNumber', 'CustomerId', 'Surname'], axis=1, inplace=True)
+df.drop(columns=['RowNumber', 'CustomerId', 'Surname'], inplace=True)
 
 ## Filtering using Age Feature using threshold
 df.drop(index=df[df['Age'] > 80].index.tolist(), axis=0, inplace=True)
